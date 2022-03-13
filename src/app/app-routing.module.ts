d0d3900bac35bc051d9f500f7c259d64197f7db7
Alvin Muniz <muniz.alvin@gmail.com>
2022-03-12 22:12:28 -0600

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomePageComponent} from "./home-page/home-page.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '',component: HomePageComponent}
+
+  // TODO define a route for the coffee form.
+  // The path should be order-form it should point to the order form component
+  // you have to create the order form component - consider using
+  // ng generate component <insert component name> to do so
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
