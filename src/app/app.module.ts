713f83e6d258a785845077f567c6da801edced2e
Alvin Muniz <muniz.alvin@gmail.com>
2022-03-13 12:02:19 -0500

    added reactive form controls ot order form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomePageComponent } from './home-page/home-page.component';
 import { OrderFormComponent } from './order-form/order-form.component';
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { OrderFormComponent } from './order-form/order-form.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/order-form/order-form.component.ts
+++ src/app/order-form/order-form.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-order-form',
@@ -7,9 +8,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OrderFormComponent implements OnInit {
 
-  constructor() { }
+  orderForm: FormGroup = new FormGroup({});
 
-  ngOnInit(): void {
+  // TODO implement a reactive form control for the fields in the html template
+  constructor(private fb: FormBuilder) {
   }
 
+  ngOnInit(): void {
+    this.orderForm = this.fb.group({
+      // For every input we have a control
+
+      // this maps to the name input field...the validator required denotes
+      // that it is a required field!
+      name: ['', Validators.required],
+
+      // we set email to a blank field
+      email: [''],
+
+      // we set the default selection of item to the Coffee value
+      item: ['Coffee', Validators.required],
+
+      // we set the size of the order form to default to Small
+      size: ['Small',Validators.required],
+
+      // we set the notes to be blank
+      notes: ['']
+    })
+  }
+
+  displayConfirmation() {
+    console.log('Confirmation');
+  }
 }
