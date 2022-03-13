f100007cf588c9c7f6c08acc887ff9c7b56fe841
Alvin Muniz <muniz.alvin@gmail.com>
2022-03-13 14:23:24 -0500

    completed tests for form fields

--- src/app/order-form/order-form.component.spec.ts
+++ src/app/order-form/order-form.component.spec.ts
@@ -2,6 +2,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { OrderFormComponent } from './order-form.component';
 import {ReactiveFormsModule} from "@angular/forms";
+import {By} from "@angular/platform-browser";
 
 describe('OrderFormComponent', () => {
   let component: OrderFormComponent;
@@ -24,4 +25,34 @@ describe('OrderFormComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should have an order form component', () => {
+    const orderForm = fixture.debugElement.nativeElement.querySelector('#order-form');
+    expect(orderForm).toBeTruthy();
+  })
+
+  it('should be set up with appropriate fields', () => {
+    const notesField = fixture.debugElement.nativeElement.querySelector('#notes-field');
+    expect(notesField).toBeTruthy();
+
+    const nameField = fixture.debugElement.nativeElement.querySelector('#name-field');
+    expect(nameField).toBeTruthy();
+
+    const emailField = fixture.debugElement.nativeElement.querySelector('#email-field');
+    expect(emailField).toBeTruthy();
+
+    const smallSizeField = fixture.debugElement.nativeElement.querySelector('#small-size-field');
+    expect(smallSizeField).toBeTruthy();
+
+    const mediumSizeField = fixture.debugElement.nativeElement.querySelector('#medium-size-field');
+    expect(mediumSizeField).toBeTruthy();
+
+    const largeSizeField = fixture.debugElement.nativeElement.querySelector('#large-size-field');
+    expect(largeSizeField).toBeTruthy();
+
+    const itemField = fixture.debugElement.nativeElement.querySelector('#item-field');
+    expect(itemField).toBeTruthy();
+
+  })
+
 });
